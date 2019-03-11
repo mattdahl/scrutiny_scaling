c730ba65b9bb9d01f25b45a3b99b26d52d15289c
Matt Dahl <matt.dahl.2013@gmail.com>
2019-03-11 15:26:54 -0400

    Splits LexisNexis queries by case category.

--- generate_lexisnexis_query.R
+++ generate_lexisnexis_query.R
@@ -1,36 +1,46 @@
-# Libs
-library(haven)
-library(readr)
-library(plyr)
+## Libs
 library(dplyr)
-library(lme4)
 
-# Load data
+## Data
 fe_data = readRDS('data/free_expression_data.rds')
 
+## Functions
+# Gets unique citations from dataframe
+# Uses Lawyer's Edition (LED) instead of the U.S. reporter, since some of the latter are missing
+get_citations = function (dataframe) {
+  return(as.character(unique(dataframe$LED)))
+}
+
+# Rewrites citations as full Lawyer's Edition citation strings
+rewrite_citations = function (citations) {
+  tokenized_citations = strsplit(citations, split = '/')
+  tokenized_citations = lapply(tokenized_citations, as.numeric)
+  citation_strings = c()
+  for (citation in tokenized_citations) {
+    citation_strings = c(citation_strings, paste('cite(', citation[1], ' L. Ed. 2d ', citation[2], ')', sep = ''))
+  }
+  return(citation_strings)
+}
+
+# Creates a search string suitable for LexisNexis
+create_query_string = function (citation_strings) {
+  return(paste(citation_strings, collapse = ' OR ', sep = ''))
+}
+
+## Script
 # Restrict to post-Grayned cases
 fe_data_after_grayned = filter(fe_data, MOSLEY == 1)
 
-# Get citations (in the format '123/0456')
-# Use Lawyer's Edition since they are complete (some U.S. citations are missing from this dataset)
-citations = as.character(unique(fe_data_after_grayned$LED))
+# Partition by content category
+cb_data = filter(fe_data_after_grayned, TTRACKS4 == 1)  # Content-based
+cn_data = filter(fe_data_after_grayned, TTRACKS4 == 2)  # Content-neutral
+lp_data = filter(fe_data_after_grayned, TTRACKS4 == 3)  # Less-protected
+nm_data = filter(fe_data_after_grayned, TTRACKS4 == 4)  # Threshold not met
 
-# Rewrite citations as strings suitable for a LexisNexis query
-tokenized_citations = strsplit(citations, split = '/')
-tokenized_citations = lapply(tokenized_citations, as.numeric)
-citation_strings = c()
-for (citation in tokenized_citations) {
-  citation_strings = c(citation_strings, paste('cite(', citation[1], ' L. Ed. 2d ', citation[2], ')', sep = ''))
-}
-
-# Break strings into chunks for easier querying (big queries are slow and possibly rate-limited)
-cs1 = citation_strings[1:100]
-cs2 = citation_strings[101:200]
-cs3 = citation_strings[201:length(citation_strings)]
-
-# Create LexisNexis search queries
-search_query1 = paste(cs1, collapse = ' OR ', sep = '')
-search_query2 = paste(cs2, collapse = ' OR ', sep = '')
-search_query3 = paste(cs3, collapse = ' OR ', sep = '')
+# Get citation strings, rewrite them, and transform them into LexisNexis query strings
+cb_query_string = create_query_string(rewrite_citations(get_citations(cb_data)))
+cn_query_string = create_query_string(rewrite_citations(get_citations(cn_data)))
+lp_query_string = create_query_string(rewrite_citations(get_citations(lp_data)))
+nm_query_string = create_query_string(rewrite_citations(get_citations(nm_data)))
 
 # Feed these queries manually into LexisNexis (no API access)
