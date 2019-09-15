42d36efd60ca6904a430983708afa7f64b7f0986
Matt Dahl <matt.dahl.2013@gmail.com>
2019-09-15 13:30:41 -0400

    Updates LexisNexis queries with BOG data.

--- scaling/generate_lexisnexis_queries.R
+++ scaling/generate_lexisnexis_queries.R
@@ -2,43 +2,41 @@
 library(dplyr)
 
 ## Data
-fe_data = readRDS('data/normalized_fe_data.rds')
+bog_data = readRDS('data/normalized_bog_data.rds')
 
 ## Functions
-# Gets unique citations from dataframe
-# Uses Lawyer's Edition (LED) instead of the U.S. reporter, since some of the latter are missing
-get_citations = function (dataframe) {
-  return(as.character(unique(dataframe$LED)))
-}
+# Rewrites citations as full Lawyer's Edition citation strings, wrapped in 'cite()' strings
+# Uses Lawyer's Edition instead of the U.S. reporter, since some of the latter are missing
+rewrite_citations = function (citation_data) {
+  citation_data$tokenized = lapply(strsplit(as.character(citation_data$led), split = '/'), as.numeric)
 
-# Rewrites citations as full Lawyer's Edition citation strings
-rewrite_citations = function (citations) {
-  tokenized_citations = strsplit(citations, split = '/')
-  tokenized_citations = lapply(tokenized_citations, as.numeric)
-  citation_strings = c()
-  for (citation in tokenized_citations) {
-    citation_strings = c(citation_strings, paste('cite(', citation[1], ' L. Ed. 2d ', citation[2], ')', sep = ''))
-  }
-  return(citation_strings)
+  citation_data$string = by(citation_data, 1:nrow(citation_data), function (citation) {
+    reporter = ''
+    if (citation$term <= 1955) {
+      reporter = ' L. Ed. '
+    }
+    else if (citation$term >= 1956) {
+      reporter = ' L. Ed. 2d '
+    }
+    paste('cite(', citation$tokenized[[1]][1], reporter, citation$tokenized[[1]][2], ')', sep = '')
+  })
+  
+  return(citation_data)
 }
 
 # Creates a search string suitable for LexisNexis
-create_query_string = function (citation_strings) {
-  return(paste(citation_strings, collapse = ' OR ', sep = ''))
+create_query_string = function (citation_data) {
+  return(paste(citation_data$string, collapse = ' OR ', sep = ''))
 }
 
 ## Script
 # Partition by content category
-cb_data = filter(fe_data, TTRACKS4 == 1)  # Content-based
-cn_data = filter(fe_data, TTRACKS4 == 2)  # Content-neutral
-lp_data = filter(fe_data, TTRACKS4 == 3)  # Less-protected
-nm_data = filter(fe_data, TTRACKS4 == 4)  # Threshold not met
+cb_data = bog_data %>% filter(cb == TRUE) %>% select(led, term) %>% distinct()  # Content-based
+cn_data = bog_data %>% filter(cn == TRUE) %>% select(led, term) %>% distinct()  # Content-neutral
 
 # Get citation strings, rewrite them, and transform them into LexisNexis query strings
-cb_query_string = create_query_string(rewrite_citations(get_citations(cb_data)))
-cn_query_string = create_query_string(rewrite_citations(get_citations(cn_data)))
-lp_query_string = create_query_string(rewrite_citations(get_citations(lp_data)))
-nm_query_string = create_query_string(rewrite_citations(get_citations(nm_data)))
+cb_query_string = create_query_string(rewrite_citations(cb_data))
+cn_query_string = create_query_string(rewrite_citations(cn_data))
 
 # Feed these queries manually into LexisNexis (no API access)
 print('Content-based cases:')
@@ -46,9 +44,3 @@ print(cb_query_string)
 
 print('Content-neutral cases:')
 print(cn_query_string)
-
-print('Less-protected cases:')
-print(lp_query_string)
-
-print('Not-met cases:')
-print(nm_query_string)
